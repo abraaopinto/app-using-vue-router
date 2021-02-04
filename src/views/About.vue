28f131155b366f9787273d2b521fd5921ed326cd
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-02-04 15:40:07 -0300

    alterações: + incluido o botão back + incluido o metodo handleback

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="handleBack" >Back</button>
   </div>
 </template>
+<script>
+export default {
+  methods: {
+    handleBack () {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
