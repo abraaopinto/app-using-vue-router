07697426bef5d4e2565b46d0e2e8c7beadce4f8d
Abraão Pinto <abraao.pinto@bancoamazonia.com.br>
2021-02-04 15:38:45 -0300

    alterações: + novas cores - removendo margin do body + adicionando nova view

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
+    <router-link to="/onepage">One Page</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
 
 <style>
+body {
+    display: block;
+    margin: 0px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: GhostWhite;
+  margin: 0px 0px;
 }
 
 #nav {
   padding: 30px;
+  background: DarkSeaGreen;
 }
 
 #nav a {
@@ -25,6 +33,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: ForestGreen;
 }
 </style>
